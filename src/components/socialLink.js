4094be9afbf1bc592da4abab7989e06f04e7dfbc
petewk <39970605+petewk@users.noreply.github.com>
2020-08-09 13:37:39 +0100

    Warning removed
    href added to facebook link to remove warning preventing deployment

--- src/components/socialLink.js
+++ src/components/socialLink.js
@@ -5,7 +5,7 @@ export default class SocialLink extends Component {
     return(
       <section id="socialLink">
         <span id="buttonBanner">
-          <div className="linkButton" id="facebook"><a href="#"><i className="fab fa-facebook-square"></i></a></div>
+          <div className="linkButton" id="facebook"><a href="https://www.facebook.com"><i className="fab fa-facebook-square"></i></a></div>
           <div className="linkButton" id="instagram"><a href="https://www.instagram.com/stevandersonkerr/"><i className="fab fa-instagram"></i></a></div>
         </span>
       </section>
