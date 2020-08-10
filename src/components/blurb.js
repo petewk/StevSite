4b5fe2c91cba61e05557c2d06c280304fcb2c8b6
petewk <39970605+petewk@users.noreply.github.com>
2020-08-10 13:28:55 +0100

    Video added
    Have added the video player with sample video, also have compressed video file to have it run more smoothly with less buffering

--- src/components/blurb.js
+++ src/components/blurb.js
@@ -6,7 +6,7 @@ export default class Blurb extends Component {
       <section id="blurbContainer">
         <h1>A BIT ABOUT OUR SERVICES</h1>
         <div>Berkshire Photographic is currently looking to expand our portfolio, so please get in touch if you'd be interested in a FREE one hour photo/video session with drone and pilot, as well as any post production included. <br /><br />
-        Yes, completely FREE.Based in Maidenhead, we create quality video media and provide aerial photo and video services to Berkshire and the surrounding area.
+        Yes, completely FREE. Based in Maidenhead, we create quality video media and provide aerial photo and video services to Berkshire and the surrounding area.
           <ul>A selection of the services we provide are: <br />
             <li>- Aerial Photography</li>
             <li>- Aerial Videography</li>
--- src/components/video.js
+++ src/components/video.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import sample from '../media/sample-compressed.m4v';
 
 export default class Video extends Component {
 
   render() {
     return(
       <div>
-        <video src="../media/sample.html"></video>
+        <video controls autoPlay preload="auto"><source  src={sample} loop="true" type="video/mp4"/></video>
       </div>
     )
   }
